550b6b8ca7ecbad7d18e8fd2bca02bb9be26ba2f
CelesteMagisteel <kamg1134@gmail.com>
2020-05-29 21:43:47 +0100

    Implemented News Model

---
+++ app/News.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Te7aHoudini\LaravelTrix\Traits\HasTrixRichText;
+
+class News extends Model {
+
+    use HasTrixRichText;
+
+    protected $fillable = ['title', 'body'];
+}
