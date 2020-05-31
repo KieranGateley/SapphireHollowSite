ee1ca0cce7ecf286ee7631ea0e5fca6fd3fe76a9
CelesteMagisteel <kamg1134@gmail.com>
2020-06-01 00:57:35 +0100

    Added dummy Manage News item to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,6 +8,9 @@
         <ul class="nav navbar-nav ml-auto">
             @if (Auth::check())
                 <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}" data-toggle="dropdown">Manage News</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
                 </li>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;"> {{ csrf_field() }}</form>
