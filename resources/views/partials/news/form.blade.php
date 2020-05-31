5322c443557999014a67d72e8ce529e46613c04c
CelesteMagisteel <kamg1134@gmail.com>
2020-06-01 00:45:59 +0100

    Updated news form labels

--- resources/views/partials/news/form.blade.php
+++ resources/views/partials/news/form.blade.php
@@ -1,12 +1,12 @@
 <form action="{{ $action }}" method="POST">
     @csrf
     <div id="form-group">
-        <label for="title-input">Post Title</label>
+        <label for="title-input">News Title</label>
         <input name="title" type="text" id="title-input" value="{{ $news->title ?? "" }}" class="form-control"/>
     </div>
     <p></p>
     <div id="form-group">
-        <label for="body-input">Post Body</label>
+        <label for="body-input">News Body</label>
         <input id="body-input" type="hidden" name="body" value="{!! $news->body ?? "" !!}">
         @include('partials.trix-bar')
         <trix-editor input="body-input" toolbar="trix-toolbar-custom"></trix-editor>
