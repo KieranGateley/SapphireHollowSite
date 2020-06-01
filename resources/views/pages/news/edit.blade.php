4a5ba1737037ba29b9bb38681261c995c5fb41d3
CelesteMagisteel <kamg1134@gmail.com>
2020-06-01 01:10:18 +0100

    Updated edit news form title to fit content

--- resources/views/pages/news/edit.blade.php
+++ resources/views/pages/news/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.container')
 
-@section('title', 'Create News Item')
+@section('title', 'Update News Item')
 
 @section('content')
     @include('partials.news.form', ['action' => route('edit_news', ['news' => $news]), 'news' => $news])
